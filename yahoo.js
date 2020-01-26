9d66e0eaa8045909e027dda66b6b7d80f2782caa
André Biseth <andre@biseth.net>
2020-01-26 21:06:37 +0100

    Testing with quotes query instead of portfolio query

--- yahoo.js
+++ yahoo.js
@@ -1,31 +1,62 @@
-var unirest = require('unirest');
+const path = require('path');
+const fs = require('fs');
+const got = require('got');
+const queryString = require('query-string');
 
-class YahooApi {    
-    constructor(userId, config){
+class YahooApi {
+    constructor(userId, config) {
         this.config = config;
         this.userId = userId;
         this.portfolio_url = 'https://query1.finance.yahoo.com/v7/finance/desktop/portfolio';
+        this.quotes_url = 'https://query2.finance.yahoo.com/v7/finance/quote';
     }
 
-//var req = unirest('GET', 'https://api.login.yahoo.com/oauth2/request_auth?client_id=dj0yJmk9bjR3Rm5OekRXdE81JmQ9WVdrOVVrUnVRVkJoTkdVbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWYy&redirect_uri=oob&response_type=code&language=en-us');
+    //var req = unirest('GET', 'https://api.login.yahoo.com/oauth2/request_auth?client_id=dj0yJmk9bjR3Rm5OekRXdE81JmQ9WVdrOVVrUnVRVkJoTkdVbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWYy&redirect_uri=oob&response_type=code&language=en-us');
 
-    get_portfolios(){
-        var req = unirest('GET', this.portfolio_url);
+    async get_portfolios() {
+        var portfolioPath = path.resolve('./portfolio_allegutta.json');
+        if (fs.existsSync(portfolioPath)) {
+            this.portfolio = JSON.parse(fs.readFileSync(portfolioPath, 'utf-8'));
+        }
 
-        req.query({
-            userId: this.userId,
+        this.portfolio.purchase_value = 0;
+
+        var tickers = '';
+        this.portfolio.forEach(element => {
+            tickers += element.symbol + ',';
+            this.portfolio.purchase_value += element.shares * element.avg_price;
         });
-        
-        req.headers({
-            'Authorization': 'Bearer ' + this.config.yahoo_token.access_token,
+        tickers = tickers.substr(0, tickers.length - 1);
+        // return this.portfolio;
+
+        const searchParams = new URLSearchParams({
+            formatted: false,
+            lang: 'nb-NO',
+            region: 'NO',
+            symbols: tickers,
+            fields: 'shortName,regularMarketPrice,regularMarketChange,regularMarketChangePercent,regularMarketVolume',
+            corsDomain: 'finance.yahoo.com',
         });
-        
-        req.end(function (res) {
-            //if (res.error) throw new Error(res.error);
-        
-            return res.body;
+
+        var quotes = await got(this.quotes_url, { searchParams })
+            .then(res => {
+                console.log(res.body);
+                return JSON.parse(res.body).quoteResponse.result;
+            }).catch(err => {
+                console.log(err.response.body);
+                return err.response.body;
+            });
+
+        this.portfolio.market_value = 0;
+        quotes.forEach(element => {
+            var result = this.portfolio.find(obj => {
+                return obj.symbol === element.symbol;
+            })
+            this.portfolio.market_value += result.shares * element.regularMarketPrice;
         });
-        
+
+        return this.portfolio;
+
     }
 }
 module.exports = {

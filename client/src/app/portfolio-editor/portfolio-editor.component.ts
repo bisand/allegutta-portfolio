9a35c193b56edbc843978bb2264f986d3ddf69a3
André Biseth <andre@biseth.net>
2020-02-02 07:44:29 +0100

    Moving code

--- client/src/app/portfolio-editor/portfolio-editor.component.ts
+++ client/src/app/portfolio-editor/portfolio-editor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IPortfolio } from '../home/home.component';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-portfolio-editor',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio-editor.component.css']
 })
 export class PortfolioEditorComponent implements OnInit {
+  responseJson: string;
+  portfolio: IPortfolio;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private api: ApiService) {
+      this.portfolio = { positions: [] };
   }
 
+  ngOnInit() {}
+
+  loadPortfolio() {
+      this.api.loadPortfolio$().subscribe(res => (this.portfolio = res));
+  }
+
+  testApi() {
+      this.api.test$().subscribe(res => (this.responseJson = res));
+  }
 }

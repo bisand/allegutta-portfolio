1a05190ea1025b69ab0a326e6c4d5ba1c803fe93
André Biseth <andre@biseth.net>
2020-02-02 17:32:49 +0100

    Fixed some menu and nav issues

--- client/src/app/nav-bar/nav-bar.component.ts
+++ client/src/app/nav-bar/nav-bar.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { faUser, faPowerOff, faEdit } from '@fortawesome/free-solid-svg-icons';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+    selector: 'app-nav-bar',
+    templateUrl: './nav-bar.component.html',
+    styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
-  isCollapsed = true;
-  faUser = faUser;
-  faEdit = faEdit;
-  faPowerOff = faPowerOff;
+    isCollapsed = true;
+    faUser = faUser;
+    faEdit = faEdit;
+    faPowerOff = faPowerOff;
 
-  constructor(public auth: AuthService) { }
+    constructor(public auth: AuthService) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
+    navigate = function(url: string) {
+        this.router.navigateByUrl(url);
+    };
 }

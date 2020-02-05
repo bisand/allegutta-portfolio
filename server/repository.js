35e8543be9b2ebb36621c52d212a09dafe0d5ec6
André Biseth <andre@biseth.net>
2020-02-05 07:52:23 +0100

    Creating tables

--- server/repository.js
+++ server/repository.js
@@ -15,20 +15,31 @@ class DataRepository {
         return db;
     }
 
-    init(params) {
-        let db = this.open();
-        db.run('CREATE TABLE langs(name text)');
-        this.close(db)
-    }
-
     close(db) {
-        db.close((err) => {
+        db.close(err => {
             if (err) {
-              console.error(err.message);
+                console.error(err.message);
             }
             console.log('Close the database connection.');
-          });
-    };
+        });
+    }
+
+    init(params) {
+        let db = this.open();
+        db.run(`CREATE TABLE [IF NOT EXISTS] portfolio (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            cash NUMERIC NOT NULL
+        );`);
+
+        db.run(`CREATE TABLE [IF NOT EXISTS] portfolio-positions (
+            id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            portfolio_id INTEGER NOT NULL,
+            symbol TEXT NOT NULL,
+            shares INTEGER NOT NULL,
+            avg_price NUMERIC NOT NULL
+        );`);
+        this.close(db);
+    }
 }
 module.exports = {
     DataRepository,

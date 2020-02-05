360ad371eeba4b693731a2aeadc9150100090f25
André Biseth <andre@biseth.net>
2020-02-05 09:56:02 +0100

    Modified tables

--- server/repository.js
+++ server/repository.js
@@ -26,17 +26,18 @@ class DataRepository {
 
     init(params) {
         let db = this.open();
-        db.run(`CREATE TABLE [IF NOT EXISTS] portfolio (
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-            cash NUMERIC NOT NULL
+        db.run(`CREATE TABLE IF NOT EXISTS "portfolio" (
+            "id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            "name"	TEXT,
+            "cash"	NUMERIC NOT NULL
         );`);
-
-        db.run(`CREATE TABLE [IF NOT EXISTS] portfolio-positions (
-            id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-            portfolio_id INTEGER NOT NULL,
-            symbol TEXT NOT NULL,
-            shares INTEGER NOT NULL,
-            avg_price NUMERIC NOT NULL
+        db.run(`CREATE TABLE IF NOT EXISTS "portfolio_positions" (
+            "id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            "portfolio_id"	INTEGER NOT NULL,
+            "symbol"	TEXT NOT NULL,
+            "shares"	INTEGER NOT NULL,
+            "avg_price"	NUMERIC NOT NULL,
+            FOREIGN KEY("portfolio_id") REFERENCES "portfolio"("id")
         );`);
         this.close(db);
     }

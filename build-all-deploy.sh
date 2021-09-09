3041e0c68aa9b64847879b8de8e20026513b0820
André Biseth <andre@biseth.net>
2021-09-09 19:22:49 +0200

    updated build script

--- build-all-deploy.sh
+++ build-all-deploy.sh
@@ -4,7 +4,7 @@ npm install
 npm run-script build
 cd client
 npm install
-ng build --aot=false --optimization --output-hashing=all --extract-css=true --source-map=false --named-chunks=false --vendor-chunk && npm run post-build
+ng build --aot=true --build-optimizer=true --optimization --output-hashing=all --extract-css=true --source-map=false --named-chunks=false --vendor-chunk && npm run post-build
 cd ..
 echo "$DOCKER_PASSWORD" | sudo docker login -u "$DOCKER_USERNAME" --password-stdin
 sudo docker build -t bisand/allegutta-portfolio .

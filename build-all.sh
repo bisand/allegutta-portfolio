b6353737669c3b7daf2da68b584347e8aca6dbcc
André Biseth <andre@biseth.net>
2022-10-10 21:43:31 +0200

    Updated packages

--- build-all.sh
+++ build-all.sh
@@ -4,5 +4,5 @@ npm install
 npm run-script build
 pushd client
 npm install
-ng build --aot=true --build-optimizer=true --optimization=true --output-hashing=all --extract-css=true --source-map=false --named-chunks=false --vendor-chunk && npm run post-build
+ng build --aot=true --build-optimizer=true --optimization=true --output-hashing=all --source-map=false --named-chunks=false --vendor-chunk && npm run post-build
 popd
